a3ebfa681e2aa4ae0d9845e40a15d09406877db9
M4cs <mabridgland@protonmail.com>
2019-07-23 10:53:30 -0400

    Push 1.0.3

---
+++ pyicns/__init__.py
@@ -0,0 +1,12 @@
+from .compiler import Compiler
+
+__version__ = "1.0.3"
+__changelog__ = """\
+    
+Version 1.0.3:
+
+- Introduces Changelog and Command To Match
+- Adds Readline Support For Folder Reading
+- Adds Custom Folder Themeing
+- Code Cleanup
+"""
--- start.py
+++ start.py
@@ -1,11 +1,12 @@
-from pyicns import compiler
+from pyicns import Compiler, __changelog__, __version__
 import os
 import requests
 import time
 import progressbar
 import webbrowser
+import readline
 
-__version__ = "1.0.2"
+readline.parse_and_bind("tab: complete")
 
 menu = f"""\
 
@@ -19,7 +20,7 @@ menu = f"""\
  ▄████▀       ▀█████▀  █▀   ████████▀   ▀█   █▀   ▄████████▀  
                                                               
 
-Welcome to Pyicns!
+Welcome to PyIcns!
 
 Current Version: {__version__}
 
@@ -34,13 +35,17 @@ This will open up a new tab with the github documentation!
 If you have any questions please contact me on twitter:
 @maxbridgIand <- that is an I yes. Not an L!
 
-1/ Install Theme Pkg
+Type 'menu' to display this menu
 
-2/ Download Themes (Coming Soon)
+1 or 'install'/ Install Theme Pkg
 
-3/ Documentation
+2 or 'download'/ Download Themes (Coming Soon)
 
-4/ Exit
+3 or 'github'/ Documentation
+
+4 or 'changelog'/ View Changelog
+
+5 or 'exit'/ Exit
 """
 
 def check_updoots():
@@ -69,42 +74,78 @@ def check_updoots():
   else:
     pass
 
-terminal = "pyicns$ "
 def main():
-  answer = input(terminal).lower()
-  if answer == "1":
-    while True:
-      print('Please Enter A Folder Location Of Your Theme Bundle:')
-      ans1 = input(terminal)
-      if os.path.exists(ans1):
-        comp = compiler.Compiler(ans1)
-        file_count = comp.file_count()
-        print('Found %s Icons For The Applications In This Folder. Is This Correct? [Y\\n]' % file_count)
-        response = input(terminal).lower()
-        if response == 'y':
-          print('Setting Theme...')
-          count = 0
-          with progressbar.ProgressBar(max_value=file_count) as bar:
-            while count <= file_count - 1:
-              comp.grab_new_app()
-              comp.set_icon()
-              bar.update(1)
-              count = count + 1
+  while True:
+    answer = input(terminal).lower()
+    if answer == "1" or answer == "install":
+      while True:
+        print('Would you like to edit a specific folder? [Y\\n]')
+        folder_ans = input(terminal).lower()
+        if folder_ans == "y":
+          print('Please Enter The Folder Location or Type Cancel To Go Back:')
+          to_be = input(terminal)
+          if to_be.lower() == "cancel":
             break
-      else:
-        print('Error! The File Does Not Exist!')
-    main()
-  elif answer[0:1] == "2":
-    print('\nComing Soon!\n')
-    main()
-  elif answer[0:1] == "3" or answer[0:5] == "github":
-    webbrowser.open_new_tab('https://github.com/M4cs/PyIcns')
-    main()
-  elif answer[0:1] == "4" or answer[0:4] == "exit":
-    exit()
-  else:
-    print('Unknown Option!')
-    main()
+          while True:
+            if os.path.exists(os.path.realpath(to_be)):
+              print('Please Enter The Folder Where Your Theme Is Located:')
+              from_here = input(terminal)
+              while True:
+                if os.path.exists(os.path.realpath(from_here)):
+                  print('Please Enter The Image File Name You Would Like To Use:')
+                  image = input(terminal)
+                  while True:
+                    if os.path.exists(os.path.realpath(from_here.replace('/', '') + '/' + image)):
+                      print('Theming Folder...')
+                      print('bins/fileicon set "{}" "{}"'.format(to_be, os.path.realpath(from_here.replace('/', '') + '/')))
+                      os.system('bins/fileicon set "{}" "{}"'.format(os.path.realpath(to_be), os.path.realpath(from_here.replace('/', '') + '/' + image)))
+                      break
+                    else:
+                      print('File Does Not Exist!')
+                break
+            else:
+              print('File Does Not Exist!')
+            break
+        print('Please Enter A Folder Location Of Your Theme Bundle or Type Cancel To Go Back:')
+        ans1 = input(terminal)
+        if ans1.lower() == "cancel":
+          print(menu)
+          break
+        if os.path.exists(ans1):
+          comp = Compiler(ans1)
+          file_count = comp.file_count()
+          print('Found %s Icons For The Applications In This Folder. Is This Correct? [Y\\n]' % file_count)
+          response = input(terminal).lower()
+          if response == 'y':
+            print('Setting Theme...')
+            count = 0
+            with progressbar.ProgressBar(max_value=file_count) as bar:
+              while count <= file_count - 1:
+                comp.grab_new_app()
+                comp.set_icon()
+                bar.update(1)
+                count = count + 1
+              break
+          elif response == 'n':
+            break
+          else:
+            print('Choose Y or N Only')
+        else:
+          print('Error! The File Does Not Exist!')
+    elif answer[0:1] == "2" or answer == "download":
+      print('\nComing Soon!\n')
+    elif answer == "3" or answer[0:5] == "github":
+      webbrowser.open_new_tab('https://github.com/M4cs/PyIcns')
+    elif answer == "4" or answer == "changelog":
+      print(__changelog__)
+    elif answer == "5" or answer[0:4] == "exit":
+      break
+    elif answer == "menu":
+      print(menu)
+    else:
+      print('Unknown Option!')
+
+terminal = "pyicns$ "
 
 if __name__ == "__main__":
   os.system('clear')
@@ -113,3 +154,4 @@ if __name__ == "__main__":
   print('Up To Date!')
   print(menu)
   main()
+  exit()
