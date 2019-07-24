139af65e30acffe299c28e636ec4a378ea93c064
M4cs <mabridgland@protonmail.com>
2019-07-23 22:24:27 -0400

    Add Setup Script and Easy Mount

---
+++ easy_mount.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+mount -uw /
+mount -uw /System/Volumes/Data
+cd /System && mkdir /AppleInternal
+cd / %% mkdir /AppleInternal
---
+++ setup.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+wget https://raw.githubusercontent.com/mklement0/fileicon/stable/bin/fileicon -O /usr/local/bin/fileicon
+chmod +x /usr/local/bin/fileicon
